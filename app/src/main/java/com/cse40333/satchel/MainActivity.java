2b811caf385aa7b7d56b5d2fda5d99c459a204fd
pdrumm <pdrumm@nd.edu>
2017-03-05 20:05:07 -0500

    Added tabbed activity

---
+++ app/src/main/java/com/cse40333/satchel/MainActivity.java
@@ -0,0 +1,35 @@
+package com.cse40333.satchel;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create & set Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Create TabLayout and add Fragments to it
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new FeedFragment(), "Feed");
+        viewPagerAdapter.addFragment(new ItemsFragment(), "Items");
+        viewPagerAdapter.addFragment(new MessagesFragment(), "Messages");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
