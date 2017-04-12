3e51c258f2fd49e54eff161ee8bfc7adef057a68
kthienem <kthienem@nd.edu>
2017-04-11 23:18:52 -0400

    Messages Updates
    Added FAB and Listview to Messages Fragment. Created initial Messages
    activity for having a layout for conersations

---
+++ app/src/main/java/com/cse40333/satchel/ConversationActivity.java
@@ -0,0 +1,47 @@
+package com.cse40333.satchel;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+/**
+ * Created by Kris on 4/11/2017.
+ */
+
+public class ConversationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation);
+
+        LinearLayout messages = (LinearLayout) findViewById(R.id.conversation_messages);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        Random r = new Random();
+        for (int i = 0; i < 20; i++) {
+
+            int temp = r.nextInt();
+            if (temp%2 == 0) {
+                LinearLayout message = (LinearLayout) inflater.inflate(R.layout.incoming_message, null);
+                ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+                messages.addView(message, lparams);
+            } else {
+                RelativeLayout message = (RelativeLayout) inflater.inflate(R.layout.outgoing_message, null);
+                ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+                messages.addView(message, lparams);
+            }
+
+        }
+    }
+}
--- app/src/main/java/com/cse40333/satchel/MessagesFragment.java
+++ app/src/main/java/com/cse40333/satchel/MessagesFragment.java
@@ -1,10 +1,13 @@
 package com.cse40333.satchel;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 
 /**
@@ -21,8 +24,26 @@ public class MessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+
+        ListView messageListView = (ListView) rootView.findViewById(R.id.message_list_view);
+
+
+        //Handler for New Message FAB
+        View.OnClickListener newConvoClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), ConversationActivity.class);
+                startActivity(intent);
+            }
+        };
+        FloatingActionButton newConvoFab = (FloatingActionButton) rootView.findViewById(R.id.add_conversation);
+        newConvoFab.setOnClickListener(newConvoClick);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_messages, container, false);
+        return rootView;
     }
 
 }
