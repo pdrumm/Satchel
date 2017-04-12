59dad1f04b3c85610499c52c1406bc66a6be2878
kthienem <kthienem@nd.edu>
2017-04-11 23:56:02 -0400

    Template Message List View
    Added some sample template list view items for the Message fragment

--- app/src/main/java/com/cse40333/satchel/ConversationActivity.java
+++ app/src/main/java/com/cse40333/satchel/ConversationActivity.java
@@ -2,6 +2,7 @@ package com.cse40333.satchel;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -21,6 +22,10 @@ public class ConversationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        setTitle("Conversation Name");
 
         LinearLayout messages = (LinearLayout) findViewById(R.id.conversation_messages);
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
--- app/src/main/java/com/cse40333/satchel/MessagesFragment.java
+++ app/src/main/java/com/cse40333/satchel/MessagesFragment.java
@@ -1,13 +1,20 @@
 package com.cse40333.satchel;
 
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -27,8 +34,22 @@ public class MessagesFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
 
+        ArrayList<String> messages = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            messages.add("");
+        }
         ListView messageListView = (ListView) rootView.findViewById(R.id.message_list_view);
+        MessagesAdapter messagesAdapter = new MessagesAdapter(getContext(), messages);
+        messageListView.setAdapter(messagesAdapter);
 
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getContext(), ConversationActivity.class);
+                startActivity(intent);
+            }
+        };
+        messageListView.setOnItemClickListener(itemClickListener);
 
         //Handler for New Message FAB
         View.OnClickListener newConvoClick = new View.OnClickListener() {
@@ -39,7 +60,7 @@ public class MessagesFragment extends Fragment {
             }
         };
         FloatingActionButton newConvoFab = (FloatingActionButton) rootView.findViewById(R.id.add_conversation);
-        newConvoFab.setOnClickListener(newConvoClick);
+//        newConvoFab.setOnClickListener(newConvoClick);
 
 
         // Inflate the layout for this fragment
